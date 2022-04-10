2883c2d5a7c3e0d5e05258bc0e7ff170a63e0945
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-04-11 01:34:32 +0200

    String output

--- roller/roller.c
+++ roller/roller.c
@@ -3,18 +3,21 @@
 #include <time.h>
 #include <stdlib.h>
 
+enum { OUTPUT_LEN=1024 };
 
-
-int roll(char roll_str[]) {
+char * roll(char roll_str[]) {
+    char* output = malloc(OUTPUT_LEN * sizeof(char));
     if (roll_str[0] == '\0') {
-        // return "ERROR: El descriptor de la tirada está vacío.\n";
-        return -1;
+        sprintf(output, "%s", "ERROR: Roll Descriptor is empty.\n");
+        return output;
+        // return -1;
     }
 
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
     srand(ts.tv_sec ^ ts.tv_nsec); // Initializes the random number generator
 
+    int chars_output = sprintf(output, "%s = [", roll_str);
 
     int result = 0;
     int sign_processed = 0;
@@ -22,6 +25,8 @@ int roll(char roll_str[]) {
     int num_dice = 0;
     int dice_faces = 0;
     int d_processed = 0;
+
+
     for (size_t pos=0; roll_str[pos]!='\0'; ++pos) {
 
         char chr = roll_str[pos];
@@ -47,25 +52,32 @@ int roll(char roll_str[]) {
             }
         } else if (chr == 'd') {
             if (d_processed) {
-                return -1;
+                // return -1;
+                sprintf(output, "%s", "ERROR: Duplicate 'd' in an element of "
+                                      "the Roll Descriptor\n");
+                return output;
             }
             d_processed = 1;
         } else if (chr == '+') {
             if (sign_processed) {
-                return -1;
+                // return -1;
+                sprintf(output, "%s", "ERROR: Sign already present in current "
+                                      "element of the Roll Descriptor\n");
+                return output;
             }
-
             if (dice_faces) {
                 for (int i=0; i<num_dice; ++i) {
-                    int roll = mult * ((rand()%dice_faces)+1);
-                    printf("(%d)", roll);
-                    result += roll;
+                    char *plus = i>0? "+" : "";
+                    int roll = (rand()%dice_faces) + 1;
+                    chars_output += sprintf(output+chars_output, "%s(%d)", plus, roll);
+                    result += mult * roll;
                 }
             } else {
-                int roll = mult * (num_dice);
-                printf("(%d)", roll);
-                result += roll;
+                int roll = num_dice;
+                chars_output += sprintf(output+chars_output, "%d", roll);
+                result += mult * roll;
             }
+            chars_output += sprintf(output+chars_output, "]+[");
 
             sign_processed = 1;
             mult = 1;
@@ -74,20 +86,24 @@ int roll(char roll_str[]) {
             d_processed = 0;
         } else if (chr == '-') {
             if (sign_processed) {
-                return -1;
+                // return -1;
+                sprintf(output, "%s", "ERROR: Sign already present in current "
+                                      "element of the Roll Descriptor\n");
+                return output;
             }
-
             if (dice_faces) {
                 for (int i=0; i<num_dice; ++i) {
-                    int roll = mult * ((rand()%dice_faces)+1);
-                    printf("(%d)", roll);
-                    result += roll;
+                    char *plus = i>0? "+" : "";
+                    int roll = (rand()%dice_faces) + 1;
+                    chars_output += sprintf(output+chars_output, "%s(%d)", plus, roll);
+                    result += mult * roll;
                 }
             } else {
-                int roll = mult * (num_dice);
-                printf("(%d)", roll);
-                result += roll;
+                int roll = num_dice;
+                chars_output += sprintf(output+chars_output, "%d", roll);
+                result += mult * roll;
             }
+            chars_output += sprintf(output+chars_output, "]-[");
 
             sign_processed = 1;
             mult = -1;
@@ -99,15 +115,18 @@ int roll(char roll_str[]) {
 
     if (dice_faces) {
         for (int i=0; i<num_dice; ++i) {
-            int roll = mult * ((rand()%dice_faces)+1);
-            printf("(%d)", roll);
-            result += roll;
+            char *plus = i>0? "+" : "";
+            int roll = (rand()%dice_faces) + 1;
+            chars_output += sprintf(output+chars_output, "%s(%d)", plus, roll);
+            result += mult * roll;
         }
     } else {
-        int roll = mult * (num_dice);
-        printf("(%d)", roll);
-        result += roll;
+        int roll = num_dice;
+        chars_output += sprintf(output+chars_output, "%d", roll);
+        result += mult * roll;
     }
+
+    chars_output += sprintf(output+chars_output, "] = %d\n", result);
 
     // printf("\n\nresult: %d\n"
     //        "sign_processed: %d\n"
@@ -118,14 +137,16 @@ int roll(char roll_str[]) {
     //        result, sign_processed, mult,
     //        num_dice, dice_faces, d_processed);
 
-    return result;
+    return output;
 }
 
 
 
 int main(int argc, char **argv) {
     if (argc == 2) {
-        printf("\n%d\n\n", roll(argv[1]));
+        char * output = roll(argv[1]);
+        printf("\n%s\n\n", output);
+        free(output);
         return 0;
     } else {
         puts("ERROR: Incorrect number of parameters. \nUsage: roller _roll_str_\n    where _roll_str_ is a valid Roll Descriptor (ex: 3d6+4-2d2)");
